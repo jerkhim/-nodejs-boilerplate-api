b2cc9ac116cc2aab08d2cbb8b365e1b86dd32ea2
unknown <YY129@ccs.local>
2024-04-07 06:35:55 +0800

    commit

--- accounts/account.model.js
+++ accounts/account.model.js
@@ -1,37 +1,41 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        email: {type: DataTypes.STRING, allowNull: false},
-        passwordHash: {type: DataTypes.STRING, allowNull: false},
-        title: {type: DataTypes.STRING, allowNull: false},
-        firstName: {type: DataTypes.STRING, allowNull: false},
-        lastName: {type: DataTypes.STRING, allowNull: false},
-        accepTerms: {type: DataTypes.STRING.BOOLEAN},
-        role: {type: DataTypes.STRING, allowNull: false},
-        verificationToken: {type: DataTypes.STRING},
-        verified: {type: DataTypes.DATE},
-        resetToken: {type: DataTypes.STRING},
-        resetTokenExpires: {type: DataTypes.DATE},
-        passwordReset: {type: DataTypes.DATE},
-        created: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
-        updated:  {type: DataTypes.DATE},
+        email: { type: DataTypes.STRING, allowNull: false },
+        passwordHash: { type: DataTypes.STRING, allowNull: false },
+        title: { type: DataTypes.STRING, allowNull: false },
+        firstName: { type: DataTypes.STRING, allowNull: false },
+        lastName: { type: DataTypes.STRING, allowNull: false },
+        acceptTerms: { type: DataTypes.BOOLEAN },
+        role: { type: DataTypes.STRING, allowNull: false },
+        verificationToken: { type: DataTypes.STRING },
+        verified: { type: DataTypes.DATE },
+        resetToken: { type: DataTypes.STRING },
+        resetTokenExpires: { type: DataTypes.DATE },
+        passwordReset: { type: DataTypes.DATE },
+        created: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
+        updated: { type: DataTypes.DATE },
         isVerified: {
             type: DataTypes.VIRTUAL,
-            get() { return !!(this.verified || this.passwordReset);}
+            get() { return !!(this.verified || this.passwordReset); }
         }
     };
-    
+
     const options = {
-        timestamps: false,
+        // disable default timestamp fields (createdAt and updatedAt)
+        timestamps: false, 
         defaultScope: {
-            attributes: { exclude: ['passwordHash']}
+            // exclude password hash by default
+            attributes: { exclude: ['passwordHash'] }
         },
         scopes: {
-            withHash: {attributes: {},}
-        }
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }        
     };
+
     return sequelize.define('account', attributes, options);
 }
